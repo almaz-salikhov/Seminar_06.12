e413b2e0ebd77d3d715d4661c477122d1c64eeae
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-07 19:17:12 +0200

    HW task 3 solved 2d variation - method

--- HomeWork/task3/Program.cs
+++ HomeWork/task3/Program.cs
@@ -2,29 +2,31 @@
 // 2 и помещает в массив таблицу кубов чисел от 1 до N. 
 // 3 Полученный массив вывести на экран.
 
-// получаем число N от пользователя
-// находим ряд от 1 до N 
-// возводим в куб каждое число 
-// наполянем массив кубами этих чисел от1 
-// выводим массив функцией 
-
 // 1 принимаем число 
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[N];
-// находим ряд от 1 до N 
-for (int i = 0; i < N; i++)
-{
-    array[i] = i + 1;
-    Console.Write(array[i] + " ");
 
-}
-Console.WriteLine();
-// Выводим на экран
-for (int i = 0; i < N; i++)
+void FillArray(int[] array)
 {
-    double stepen = 3;
-    array[i] = (int)Math.Pow(array[i], stepen);
-    Console.Write((array[i] + " "));
-
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = i + 1;
+        Console.Write(array[i] + " ");
+    }
 }
+void PrintStepenArray(int[] array)
+{
+    for (int i = 0; i < N; i++)
+    {
+        double stepen = 3;
+        array[i] = (int)Math.Pow(array[i], stepen);
+        Console.Write((array[i] + " "));
+
+    }
+}
+
+FillArray(array); // создаем массив от 1 до N 
+Console.WriteLine(); // отделяем визуально 
+PrintStepenArray(array);// Выводим на экран
+
