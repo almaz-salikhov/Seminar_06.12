d525b48d56ceb575d953ee40fb85700c6888b317
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-08 20:26:17 +0200

    Modified task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,7 +3,6 @@
     for (int i = 0; i < len; i++)
     {   
         arr[i] = new Random().Next(1, 10+1); // new — создает объект
-       
     }
 }
 
@@ -12,33 +11,25 @@ int N = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[N]; // все, что вводит пользователь, важно проверять на валидность!
 
 //2
-FillArray(array, N); // функция, которая 
-/*
-for (int i = 0; i < N; i++)
-{
-    array[i] = new Random().Next(1, 10+1); // new — создает объект
-}
-// имя, возвращаемое значение и параметры
-*/
+FillArray(array, N);
 
 //3
 int sum = 0;
-float result = 0;
-int countOfEvens = 0;
+float result = 0; // float, чтобы сохранить дробное значение 
+int countOfEvens = 0; // доп переменная для подсчета количества четных чисел
 
-for (int i = 0; i < N; i++)
+for (int i = 0; i < N; i++) // перебор элементов, удовлетворяющих условию
 {
     // 1 обратиться к четному
     if(array[i] % 2 == 0)
     {
-        // 2 прибавить к сумме 
+        // 2 прибавить к сумме, "накопить"
         sum += array[i];
         // 3 вести подсчет четных элементов
-        countOfEvens++;
-
+        countOfEvens++; // even — четное, odd - нечетное
     }
 }
 
-result = (float)sum / countOfEvens;
+result = (float)sum / countOfEvens; // без float он "округлит" значение, обрезав после запятой 
 Console.WriteLine(result);
 
